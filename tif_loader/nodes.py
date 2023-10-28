0460ba189a676cbaf0a34be682f98cea94a465c5
oanegros <oanegros@gmail.com>
2023-10-28 10:09:16 +0200

    correct rotations to have index 0 at 0,0, using flip faces for bottoms

--- tif_loader/nodes.py
+++ tif_loader/nodes.py
@@ -3,13 +3,14 @@ import numpy as np
 
 #initialize gridbox node group
 def gridbox_node_group():
-    gridbox= bpy.data.node_groups.new(type = 'GeometryNodeTree', name = "gridbox.002")
+    gridbox= bpy.data.node_groups.new(type = 'GeometryNodeTree', name = "agridbox2")
     links = gridbox.links
     
+    # -- get IO --
     #gridbox inputs
     #input Vector
     gridbox.inputs.new('NodeSocketVector', "size (m)")
-    gridbox.inputs[-1].default_value = (10.0, 10.0, 10.0)
+    gridbox.inputs[-1].default_value = (13.0, 10.0, 6.0)
     gridbox.inputs[-1].min_value = 0.0
     gridbox.inputs[-1].max_value = 10000000.0
     gridbox.inputs[-1].attribute_domain = 'POINT'
@@ -37,51 +38,138 @@ def gridbox_node_group():
     
      #node Group Input
     group_input = gridbox.nodes.new("NodeGroupInput")
-    group_input.location = (-400,0)
+    group_input.location = (-800,0)
     
     #output Geometry
     gridbox.outputs.new('NodeSocketGeometry', "Geometry")
     gridbox.outputs[0].attribute_domain = 'POINT'
     group_output = gridbox.nodes.new("NodeGroupOutput")
-    group_output.location = (1200,0)
+    group_output.location = (2000,0)
     
     join_geo = gridbox.nodes.new("GeometryNodeJoinGeometry")
-    join_geo.location = (1030,0)
+    join_geo.location = (1830,0)
 
-    transforms = []
+    ## -- process IO --
+
+    loc_0 =  gridbox.nodes.new("ShaderNodeVectorMath")
+    loc_0.operation = "MULTIPLY"
+    loc_0.location = (-600, -100)
+    loc_0.label = "location 0,0,0"
+    links.new(group_input.outputs.get("size (m)"), loc_0.inputs[0])
+    loc_0.inputs[1].default_value = (-0.5,-0.5,0)
+    
+    loc_max =  gridbox.nodes.new("ShaderNodeVectorMath")
+    loc_max.operation = "MULTIPLY"
+    loc_max.location = (-450, -100)
+    loc_max.label = "location max val"
+    links.new(group_input.outputs.get("size (m)"), loc_max.inputs[0])
+    loc_max.inputs[1].default_value = (0.5,0.5,1)
+    
+    µm_ticks_float =  gridbox.nodes.new("ShaderNodeVectorMath")
+    µm_ticks_float.operation = "DIVIDE"
+    µm_ticks_float.location = (-600, 100)
+    µm_ticks_float.label = "float-nr of ticks"
+    links.new(group_input.outputs.get("size (µm)"), µm_ticks_float.inputs[0])
+    links.new(group_input.outputs.get("µm per tick"), µm_ticks_float.inputs[1])
+
+    µm_ticks_int =  gridbox.nodes.new("ShaderNodeVectorMath")
+    µm_ticks_int.operation = "FLOOR"
+    µm_ticks_int.location = (-450, 100)
+    µm_ticks_int.label = "nr of ticks"
+    links.new(µm_ticks_float.outputs[0], µm_ticks_int.inputs[0])
+    
+    µm_ticks_int =  gridbox.nodes.new("ShaderNodeVectorMath")
+    µm_ticks_int.operation = "CEIL"
+    µm_ticks_int.location = (-450, 100)
+    µm_ticks_int.label = "nr of ticks"
+    links.new(µm_ticks_float.outputs[0], µm_ticks_int.inputs[0])
+    
+    size_overshoot =  gridbox.nodes.new("ShaderNodeVectorMath")
+    size_overshoot.operation = "MULTIPLY"
+    size_overshoot.location = (-300, 0)
+    size_overshoot.label = "overshoot size vec"
+    links.new(µm_ticks_int.outputs[0], size_overshoot.inputs[0])
+    links.new(group_input.outputs.get("m per µm"), size_overshoot.inputs[1])
+    
+    size_overshootXYZ =  gridbox.nodes.new("ShaderNodeSeparateXYZ")
+    size_overshootXYZ.location = (-150, 0)
+    size_overshootXYZ.label = "grid size"
+    links.new(size_overshoot.outputs[0], size_overshootXYZ.inputs[0])
+    
+    
+    # make principal box
+    finals = []
     for sideix, side in enumerate(['bottom', 'top']):
-        for axix, ax in enumerate(['xy','yz','xz']):
+        for axix, ax in enumerate(['xy','yz','zx']):
             grid = gridbox.nodes.new("GeometryNodeMeshGrid")
             grid.label = ax+"_"+side
             grid.name = ax+"_"+side+"_grid"
             
+            for which, axis in enumerate("xyz"):
+                if axis in ax:
+                    links.new(size_overshootXYZ.outputs[which], grid.inputs[ax.find(axis)])
+                    
+            
             transform = gridbox.nodes.new("GeometryNodeTransform")
             links.new(grid.outputs[0], transform.inputs[0])
+
+
+            if side == "top":
+                pretransform = gridbox.nodes.new("ShaderNodeVectorMath")
+                pretransform.operation = 'MULTIPLY'
+                links.new(group_input.outputs.get("size (m)"), pretransform.inputs[0])
+                links.new(pretransform.outputs[0], transform.inputs.get("Translation"))
+                
+                # shift tops to the correct plane (out-of-axis) and calc rotation
+                shift = np.array([float(axis not in ax) for axis in "xyz"]) 
+                pretransform.inputs[1].default_value = tuple(shift)
+            else:
+                pretransform = gridbox.nodes.new("GeometryNodeFlipFaces")
+                links.new(grid.outputs[0], pretransform.inputs[0])
+                links.new(pretransform.outputs[0], transform.inputs[0])
             
+            rot = [0,0,0]
+            if ax == "yz":
+                rot = [0.5,0,0.5]
+            elif ax == 'zx':
+                rot = [0,-0.5,-0.5]
+            transform.inputs.get("Rotation").default_value = tuple(np.array(rot)*np.pi)
+#            rot = shift/2
+##            if np.array_equal(shift,np.array([0,0,0])):
+##                shift = np.array([0,1,0]) #flip bottom
+#            if side == 'top':
+#                rot += np.array([0, 0, 0.5])
+#                rot = rot * -1
+#            rot = rot * np.pi
+#            transform.inputs.get("Rotation").default_value = tuple(rot)
             
-            translocate_box = gridbox.nodes.new("ShaderNodeVectorMath")
-            translocate_box.operation = 'MULTIPLY'
-            links.new(group_input.outputs.get("size (m)"), translocate_box.inputs[0])
-            links.new(translocate_box.outputs[0], transform.inputs.get("Translation"))
+            # translocate 0,0 to be accurate for each
+            bbox = gridbox.nodes.new("GeometryNodeBoundBox")
+            links.new(transform.outputs[0],bbox.inputs[0])
             
-            shift = np.array([float(axis not in ax) for axis in "xyz"]) / 2
-            if side == 'bottom':
-                shift *= -1
-            shift += np.array([0, 0, 0.5])
-            translocate_box.inputs[1].default_value = tuple(shift)
+            top = gridbox.nodes.new("ShaderNodeVectorMath")
+            top.operation = "MULTIPLY"
+            links.new(bbox.outputs[1], top.inputs[0])
+            top.inputs[1].default_value =(1,1,1)
+            if side == 'top':
+                top.inputs[1].default_value = tuple(np.array([float(axis in ax) for axis in "xyz"]))
             
-            if np.array_equal(shift,np.array([0,0,0])):
-                shift = np.array([0,1,0])
-            rot = shift * np.pi
-            transform.inputs.get("Rotation").default_value = tuple(rot)
+            find_0 = gridbox.nodes.new("ShaderNodeVectorMath")
+            find_0.operation = "SUBTRACT"
+            links.new(loc_0.outputs[0], find_0.inputs[0])
+            links.new(top.outputs[0], find_0.inputs[1])
             
+            set_pos = gridbox.nodes.new("GeometryNodeSetPosition")
+            links.new(transform.outputs[0], set_pos.inputs[0])
+            links.new(find_0.outputs[0], set_pos.inputs[-1])
+
             
-            for locix,node in enumerate([grid,translocate_box, transform]):
+            for locix,node in enumerate([grid,pretransform, transform,bbox,top, find_0, set_pos]):
                 nodeix = (sideix*3)+axix
                 node.location = (200*locix, 300*2.5 + nodeix * -300)
-            transforms.append(transform)
-    for transform in reversed(transforms):            
-        links.new(transform.outputs[0], join_geo.inputs[0])
+            finals.append(set_pos)
+    for final in reversed(finals):            
+        links.new(final.outputs[0], join_geo.inputs[0])
     links.new(join_geo.outputs[0],group_output.inputs[0])
 
     #initialize gridbox nodes
