08c0ee656f76161888ba1229775d6a1422c7d589
oanegros <oanegros@gmail.com>
2023-10-29 08:54:47 +0100

    select in-grid verts node group

--- tif_loader/nodes.py
+++ tif_loader/nodes.py
@@ -199,5 +199,110 @@ def scalebox_node_group():
 
     return scalebox
 
-scalebox = scalebox_node_group()
+# scalebox = scalebox_node_group()
+import bpy
+
+def grid_verts_node_group():
+    grid_verts= bpy.data.node_groups.new(type = 'GeometryNodeTree', name = "grid_verts")
+    links = grid_verts.links
+    
+    # -- get IO --
+    #grid_verts inputs
+    #input Vector
+    grid_verts.inputs.new('NodeSocketVector', "size (m)")
+    grid_verts.inputs[-1].default_value = (13.0, 10.0, 6.0)
+    grid_verts.inputs[-1].min_value = 0.0
+    grid_verts.inputs[-1].max_value = 10000000.0
+    grid_verts.inputs[-1].attribute_domain = 'POINT'
+
+    #node Group Input
+    group_input = grid_verts.nodes.new("NodeGroupInput")
+    group_input.location = (-800,0)
+    
+    pos = grid_verts.nodes.new("GeometryNodeInputPosition")
+    pos.location = (-600, 130)
+    
+    posXYZ =  grid_verts.nodes.new("ShaderNodeSeparateXYZ")
+    posXYZ.location = (-400, 130)
+    posXYZ.label = "posXYZ"
+    links.new(pos.outputs[0], posXYZ.inputs[0])
+    
+    compnodes = [[],[],[]]
+    for ix, side in enumerate(['min', 'max']):
+        loc =  grid_verts.nodes.new("ShaderNodeVectorMath")
+        loc.operation = "MULTIPLY"
+        loc.location = (-600, -200 * ix)
+        loc.label = "location 0,0,0"
+        links.new(group_input.outputs.get("size (m)"), loc.inputs[0])
+        if side == 'min':
+            loc.inputs[1].default_value = (-0.5,-0.5,0)
+        else: 
+            loc.inputs[1].default_value = (0.5,0.5,1)
+        locXYZ =  grid_verts.nodes.new("ShaderNodeSeparateXYZ")
+        locXYZ.location = (-400, -130*ix)
+        locXYZ.label = side + "XYZ"
+        links.new(loc.outputs[0], locXYZ.inputs[0])
+        
+        
+        for axix, ax in enumerate("XYZ"):
+            # element wise compare
+            compare = grid_verts.nodes.new("FunctionNodeCompare")
+            compare.data_type = 'FLOAT'
+            compare.operation = 'EQUAL'
+            compare.mode = 'ELEMENT'
+            compare.label = "value on " + side + " in " + ax
+            compare.location = (-200, ((ix*3)+axix) * -200 +300)
+            links.new(posXYZ.outputs[axix], compare.inputs[0])
+            links.new(locXYZ.outputs[axix], compare.inputs[1])
+            compnodes[axix].append(compare)
+    
+    ornodes = []
+    for axix, ax in enumerate("XYZ"):
+        ornode = grid_verts.nodes.new("FunctionNodeBooleanMath")
+        ornode.operation = 'OR'
+        for nix,compnode in enumerate(compnodes[axix]):
+            links.new(compnode.outputs[0], ornode.inputs[nix])
+        ornode.location = (0, (axix) * -200 +100)
+        ornode.label = "vert in min or max of " + ax
+        ornodes.append(ornode)
+    
+    andnodes = []
+    for i in range(3):
+        andnode = grid_verts.nodes.new("FunctionNodeBooleanMath")
+        andnode.operation = 'AND'
+        links.new(ornodes[i].outputs[0], andnode.inputs[0])
+        links.new(ornodes[i-1].outputs[0], andnode.inputs[1])
+        andnode.location = (200, i * -200 +100)
+        andnodes.append(andnode)
+    
+    ornodes2 = []
+    ornode = grid_verts.nodes.new("FunctionNodeBooleanMath")
+    ornode.operation = 'OR'
+    links.new(andnodes[0].outputs[0], ornode.inputs[0])
+    links.new(andnodes[1].outputs[0], ornode.inputs[1])
+    ornode.location = (400, 100)
+    ornodes2.append(ornode)
+    
+    ornode = grid_verts.nodes.new("FunctionNodeBooleanMath")
+    ornode.operation = 'OR'
+    links.new(andnodes[1].outputs[0], ornode.inputs[0])
+    links.new(andnodes[2].outputs[0], ornode.inputs[1])
+    ornode.location = (400, -100)
+    ornodes2.append(ornode)
+
+    nornode = grid_verts.nodes.new("FunctionNodeBooleanMath")
+    nornode.operation = 'NOR'
+    links.new(ornodes2[0].outputs[0], nornode.inputs[0])
+    links.new(ornodes2[1].outputs[0], nornode.inputs[1])
+    nornode.location = (600, 100)
+
+    #output Geometry
+    grid_verts.outputs.new('NodeSocketBool', "Boolean")
+    grid_verts.outputs[0].attribute_domain = 'POINT'
+    group_output = grid_verts.nodes.new("NodeGroupOutput")
+    group_output.location = (800,100)
+    links.new(nornode.outputs[0], group_output.inputs[0])
+    return grid_verts
+
+# grid_verts = grid_verts_node_group()
 
