57f24dd6da0c64d9264a8003aa36779ceebd6f3f
oanegros <oanegros@gmail.com>
2023-11-05 17:46:10 +0100

    do #20

--- tif_loader/load.py
+++ tif_loader/load.py
@@ -195,6 +195,7 @@ def preset_environment():
     bpy.context.scene.cycles.preview_samples = 32
     bpy.context.scene.cycles.samples = 64
     bpy.context.scene.view_settings.view_transform = 'Standard'
+    bpy.context.scene.eevee.volumetric_end = 1000
     try:
         bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (0, 0, 0, 1)
     except:
