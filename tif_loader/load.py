4ec67b12d0318bc0ff7c253518744290336d74cf
oanegros <oanegros@gmail.com>
2023-11-06 16:00:42 +0100

    preset more eevee samples to accomodate longer clipping length

--- tif_loader/load.py
+++ tif_loader/load.py
@@ -195,7 +195,9 @@ def preset_environment():
     bpy.context.scene.cycles.preview_samples = 32
     bpy.context.scene.cycles.samples = 64
     bpy.context.scene.view_settings.view_transform = 'Standard'
-    bpy.context.scene.eevee.volumetric_end = 1000
+    bpy.context.scene.eevee.volumetric_end = 500
+    bpy.context.scene.eevee.taa_samples = 64
+
     bpy.context.space_data.shading.use_scene_world = True
 
     try:
